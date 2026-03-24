4015616c17381c9f7794ceaf1b85f17e67997cd2
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-03-24 17:55:06 +0600

    make parseArgs builder like class,

--- src/cit/cli.py
+++ src/cit/cli.py
@@ -1,2 +1,16 @@
+from cli import ParseArgs
+
 def main():
-    print("cit is running!")
+    parse_args = ParseArgs()
+    parse_args.add_init_command()\
+            .add_add_command()\
+            .add_commit_command()\
+            .add_checkout_command()\
+            .add_branch_command()\
+            .add_log_command()\
+            .add_status_command()
+    try:
+        command = parse_args.parse_cmd()
+        command.execute(repo=None, args=parse_args.parse())
+    except Exception as e:
+        print(f"Error: {e}")
--- src/cit/core/parse_args.py
+++ src/cit/core/parse_args.py
@@ -1,8 +1,10 @@
 
 import argparse
+from typing import Self
 
 from cit import InitCommand
 from cit import NoCommand
+from cit import Command
 
 
 class ParseArgs:
@@ -10,59 +12,72 @@ class ParseArgs:
         self.parser = argparse.ArgumentParser(description="Cit - A simple git clone written in python!")
         self.subparsers = self.parser.add_subparsers(dest="command", help="Available commands")
         self.COOMMANDS = {}
+        self.args = None
 
 
-    def add_init_command(self):
+    def add_init_command(self) -> Self:
         arg = "init"
         self.subparsers.add_parser(arg, help="Initialize a new repository")
         self.COOMMANDS[arg] = InitCommand()
+        return self
 
 
-    def add_add_command(self):
+    def add_add_command(self) -> Self:
         arg = "add"
         add_parser = self.subparsers.add_parser(arg, help="Add file contents to the index")
         add_parser.add_argument("paths", nargs="+", help="Files and directories to add")
         self.COOMMANDS[arg] = None
+        return self
 
 
-    def add_commit_command(self):
+    def add_commit_command(self) -> Self:
         arg = "commit"
         commit_parser = self.subparsers.add_parser(arg, help="Record changes to the repository")
         commit_parser.add_argument("-m", "--message", help="Commit message", required=True)
         self.COOMMANDS[arg] = None
+        return self
 
 
-    def add_checkout_command(self):
+    def add_checkout_command(self) -> Self:
         arg = "checkout"
         checkout_parser = self.subparsers.add_parser(arg, help="Move/Create a new branch")
         checkout_parser.add_argument("branch", help="Branch to switch to")
         checkout_parser.add_argument("-b", "--create-branch", action="store_true", help="Create and switch to a new branch")
         self.COOMMANDS[arg] = None
+        return self
 
 
-    def add_branch_command(self):
+    def add_branch_command(self) -> Self:
         arg = "branch"
         branch_parser = self.subparsers.add_parser(arg, help="List or manage branches")
         branch_parser.add_argument("name", nargs="?")
         branch_parser.add_argument("-d", "--delete", action="store_true", help="Delete the branch")
         self.COOMMANDS[arg] = None
+        return self
 
 
-    def add_log_command(self):
+    def add_log_command(self) -> Self:
         arg = "log"
         log_parser = self.subparsers.add_parser(arg, help="Show commit logs")
         log_parser.add_argument("-n", "--max-count", type=int, default=10, help="Limit commits shown")
         self.COOMMANDS[arg] = None
+        return self
 
 
-    def add_status_command(self):
+    def add_status_command(self) -> Self:
         arg = "status"
         self.subparsers.add_parser(arg, help="Show the working tree status")
         self.COOMMANDS[arg] = None
+        return self
+
+    def parse(self) -> argparse.Namespace:
+        if self.args is None:
+            self.args = self.parser.parse_args()
+        return self.args
 
 
-    def parse_cmd(self):
-        args = self.parser.parse_args()
+    def parse_cmd(self) -> Command:
+        args = self.parse()
         command = self.COOMMANDS[args]
         if not args.command or not command:
             return NoCommand(parser=self.parser)
