ad64c442bc80966a3d838d7e5426a03e993f0a71
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-03-25 20:29:15 +0600

    add commit class

--- src/cit/core/cmd_args/commit_command.py
+++ src/cit/core/cmd_args/commit_command.py
@@ -1,9 +1,15 @@
 from argparse import Namespace
 
 from cit.core.cmd_args.command import Command
+from cit.core.repository import Repository
 
 
 class CommitCommand(Command):
     
-    def execute(self, repo, args: Namespace):
-        print(args)
+    def execute(self, repo: Repository, args: Namespace):
+        if not repo.git_dir.exists():
+            print("Not a git repository")
+            return
+
+        author = args.author or "<user@cithub.com>"
+        repo.commit(args.message, author)
