b04b2961e5f1414ce26240fdb0e18cd6d6b4ca6b
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-03-25 12:57:13 +0600

    add init functionality

--- src/cit/core/cmd_args/init_command.py
+++ src/cit/core/cmd_args/init_command.py
@@ -6,4 +6,5 @@ from cit.core.cmd_args.command import Command
 class InitCommand(Command):
     
     def execute(self, repo, args: Namespace):
-        print(args)
+        if not repo.init():
+            print("Repository already exists")
